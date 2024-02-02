0701b7db7f8a51895d8132aa0516e62726d8a726
Yu seungbeom <dldkdlelqnzo@gmail.com>
2024-02-02 15:42:16 +0900

    gitignore에 /.idea/* 추가 & OrderController 추가

---
+++ src/main/java/com/brokurly/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.brokurly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+    @GetMapping("/checkout")
+    public String showCheckout() {
+        return "order/checkout";
+    }
+}
